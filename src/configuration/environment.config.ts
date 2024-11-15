ba3f97a68e30f0da975a6ee41ff7eca80b9e13f0
charlis.monhol <charlis.monhol@el.com.br>
2024-11-15 17:38:03 -0300

    configuracao da routes

--- src/configuration/environment.config.ts
+++ src/configuration/environment.config.ts
@@ -1,5 +1,4 @@
 import dotenv from "dotenv";
-
 const NODE_ENV = process.env?.NODE_ENV || "development";
 
 console.log("Ambiente configurado:", NODE_ENV);
--- src/configuration/express.config.ts
+++ src/configuration/express.config.ts
@@ -1,8 +1,10 @@
 import express, { Application } from "express";
+import routes from "./routes.config";
 
 const ExpressConfig = (): Application => {
   const app = express();
   app.use(express.json());
+  app.use(routes);
   return app;
 };
 
