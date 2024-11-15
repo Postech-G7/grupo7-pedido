75a32bba29d9f21cb6af3bca1cd7df50424a1d65
charlis.monhol <charlis.monhol@el.com.br>
2024-11-15 17:15:37 -0300

    configuração do dotenv

---
+++ src/configuration/environment.config.ts
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+
+const NODE_ENV = process.env?.NODE_ENV || "development";
+
+console.log("Ambiente configurado:", NODE_ENV);
+export const Environment = () => {
+  dotenv.config({ path: `src/configurations/environments/${NODE_ENV}.env` });
+};
--- src/configuration/server.config.ts
+++ src/configuration/server.config.ts
@@ -1,9 +1,10 @@
 import ExpressConfig from "./express.config";
+import { Environment } from "./environment.config";
 
-const PORT = 3000;
+Environment();
 
 const app = ExpressConfig();
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port" + process.env.PORT);
 });
